922b2b2dc59e2af05d25c599f8d29f6b982b2585
vandalist <feddevanderlist@gmail.com>
2025-09-25 11:39:51 +0200

    #1 fix testen and test id added

--- tests/icon.spec.ts
+++ tests/icon.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('check that icons exist', async ({ page }) => {
     await page.goto('http://localhost:8080/');
-    await expect(page.getByRole('row', { name: '► Zombie Hunter Zombie Hunter 2 100 84 800 10 20 soldiers Soldier-Hunter' }).getByRole('img')).toBeVisible();
+    await expect(page.getByRole('row', { name: '► Zombie Hunter Zombie Hunter 2 100 84 800 10 20 soldiers Soldier-Hunter' }).getByTestId('icon-Zombie Hunter')).toBeVisible();
 });
