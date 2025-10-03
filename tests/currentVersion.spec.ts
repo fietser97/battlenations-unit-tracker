ce644b7c40f297013ef0d78054bf627c07e4d9ed
vandalist <feddevanderlist@gmail.com>
2025-10-03 14:16:01 +0200

    prepare for version update mid-October

--- tests/currentVersion.spec.ts
+++ tests/currentVersion.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('current version button test', async ({ page }) => {
     await page.goto('http://localhost:8080/');
     await page.getByTestId('current-version-btn').click();
     await expect(page.getByTestId('other-filter-wrapper').getByRole('textbox')).toHaveValue('(none), How to Kill People From Quite a Long Way Away, Weapons Factory, Rocket Factory, SpecOps Center, Chemical Weapons Lab, Gun Foundry, Raptor Ranch, Boar Pen, Mammoth Pen, Sandworm Ranch, Infection Test Facility, Armor ShopGun Foundry, Armor Shop, Armor ShopChemical Weapons Lab, Reef Bandit Armory');
-    await expect(page.locator('#unlock-range-display')).toContainText('Level: 0 - 46');
+    await expect(page.locator('#unlock-range-display')).toContainText('Level: 0 - 49');
     const firstCell = await page.getByRole('cell', { name: 'Trooper', exact: true }).first().textContent();
     expect(firstCell?.trim()).toBe('Trooper');
 });
